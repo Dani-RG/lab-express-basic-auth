ef26a1376205ddbe13c1ee7a849ca8a57d2bffd7
Daniel-RG <21danielrondong@gmail.com>
2023-01-23 23:19:25 +0100

    Iteratimon 1, 2, 3 done, Lab done

--- app.js
+++ app.js
@@ -9,11 +9,23 @@ require('./db');
 // https://www.npmjs.com/package/express
 const express = require('express');
 
+const path = require('path');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
 
 const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
@@ -24,12 +36,32 @@ const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerC
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
+// For deployment
+app.set('trust proxy', 1);
+app.use(
+  session({
+    name: 'basic-auth',
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 2592000000 // 30 days in milliseconds
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL
+    })
+  })
+)
+
 // 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
+app.use('/', indexRouter);
+app.use('/', usersRouter);
+app.use('/auth', authRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
 module.exports = app;
-
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-const router = require("express").Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  const user = req.session.currentUser;
+  res.render('index', { title: 'Basic auth', user });
 });
 
 module.exports = router;
